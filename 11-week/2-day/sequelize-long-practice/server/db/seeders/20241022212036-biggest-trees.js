1e7e861aeb791b0102f02090ce2dafe87981cc62
Zaviar Brown <brown.zaviar@gmail.com>
2024-10-22 14:45:09 -0700

    Seeds are working!

---
+++ 11-week/2-day/sequelize-long-practice/server/db/seeders/20241022212036-biggest-trees.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const { Tree } = require('../models');
+
+const treeSeeds = [
+    {
+        tree: 'General Sherman',
+        location: 'Sequoia National Park',
+        heightFt: 274.9,
+        groundCircumferenceFt: 102.6,
+    },
+    {
+        tree: 'General Grant',
+        location: 'Kings Canyon National Park',
+        heightFt: 268.1,
+        groundCircumferenceFt: 107.5,
+    },
+    {
+        tree: 'President',
+        location: 'Sequoia National Park',
+        heightFt: 240.9,
+        groundCircumferenceFt: 93,
+    },
+    {
+        tree: 'Lincoln',
+        location: 'Sequoia National Park',
+        heightFt: 255.8,
+        groundCircumferenceFt: 98.3,
+    },
+    {
+        tree: 'Stagg',
+        location: 'Private Land',
+        heightFt: 243,
+        groundCircumferenceFt: 109,
+    },
+];
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        await Tree.bulkCreate(treeSeeds, { validate: true });
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('Trees', {
+            tree: [
+                'General Sherman',
+                'General Grant',
+                'President',
+                'Lincoln',
+                'Stagg',
+            ],
+        });
+    },
+};
