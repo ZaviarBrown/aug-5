83088e74642050756f40fdeb52a7460b5b24db9f
Zaviar Brown <brown.zaviar@gmail.com>
2024-10-22 15:15:44 -0700

    Insects of Phase 3 have been migrated and modeled

---
+++ 11-week/2-day/sequelize-long-practice/server/db/models/insect.js
@@ -0,0 +1,53 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Insect extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Insect.init(
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isTitleCased(str) {
+                        const strArr = str.split(' ');
+                        strArr.forEach((word) => {
+                            if (word.at(0) !== word[0].toUpperCase()) {
+                                throw new Error('Title pls 😡');
+                            }
+                        });
+                    },
+                },
+            },
+            description: DataTypes.STRING,
+            territory: DataTypes.STRING,
+            fact: {
+                type: DataTypes.STRING,
+                validate: {
+                    len: [0, 240],
+                },
+            },
+            millimeters: {
+                type: DataTypes.FLOAT,
+                allowNull: false,
+                validate: {
+                    min: 0,
+                },
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Insect',
+        }
+    );
+    return Insect;
+};
